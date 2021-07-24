b31326075f40bcb5aa22b23cfc51a22333637f76
John Westhoff <johnjwesthoff@gmail.com>
2021-07-24 18:40:03 -0400

    more work on backend

--- backend/src/board.rs
+++ backend/src/board.rs
@@ -43,13 +43,84 @@ impl Board {
         self.add_piece_mirror(piece, rank, file);
         self.add_piece_mirror(piece, rank, STANDARD_BOARD_SIZE - file - 1);
     }
-    pub fn get_piece_at(&self, rank: BigInt, file: BigInt) -> Option<&Piece> {
+    pub fn get_piece_at(&self, rank: &BigInt, file: &BigInt) -> Option<&Piece> {
         for p in &self.pieces {
-            if p.rank == rank && p.file == file {
+            if &p.rank == rank && &p.file == file {
                 return Some(&p);
             }
         }
         None
+    }
+    fn get_collision(
+        &self,
+        from_rank: &BigInt,
+        from_file: &BigInt,
+        to_rank: &BigInt,
+        to_file: &BigInt,
+    ) -> bool {
+        let dr = match (to_rank - from_rank).sign() {
+            num_bigint::Sign::Minus => -1,
+            num_bigint::Sign::NoSign => 0,
+            num_bigint::Sign::Plus => 1,
+        };
+        let df = match (to_file - from_file).sign() {
+            num_bigint::Sign::Minus => -1,
+            num_bigint::Sign::NoSign => 0,
+            num_bigint::Sign::Plus => 1,
+        };
+        let mut rank = from_rank.clone();
+        let mut file = from_file.clone();
+        rank += dr;
+        file += df;
+        while &rank != to_rank && &file != to_file {
+            if let Some(_) = self.get_piece_at(&rank, &file) {
+                return false;
+            }
+            rank += dr;
+            file += df;
+        }
+        true
+    }
+    pub fn is_move_legal(
+        &self,
+        from_rank: &BigInt,
+        from_file: &BigInt,
+        to_rank: &BigInt,
+        to_file: &BigInt,
+    ) -> bool {
+        if from_rank == to_rank && from_file == to_file {
+            return false;
+        }
+        if let Some(p) = self.get_piece_at(from_rank, from_file) {
+            match p.piece {
+                PieceType::Pawn(_) => self.is_pawn_move_legal(p, to_rank, to_file),
+                PieceType::Knight(_) => todo!(),
+                PieceType::Bishop(_) => todo!(),
+                PieceType::Rook(_) => todo!(),
+                PieceType::Queen(_) => todo!(),
+                PieceType::King(_) => todo!(),
+            }
+        } else {
+            false //no piece there so not legal
+        }
+    }
+    fn is_pawn_move_legal(&self, p: &Piece, to_rank: &BigInt, to_file: &BigInt) -> bool {
+        if &p.file == to_file && to_rank == &(&p.rank + 1) {
+            if let None = self.get_piece_at(to_rank, to_file) {
+                return true;
+            }
+        }
+        if &p.file == to_file
+            && to_rank == &(&p.rank + 2)
+            && (&p.rank == &0.into() || &p.rank == &(STANDARD_BOARD_SIZE - 1).into())
+        {
+            if let None = self.get_piece_at(to_rank, to_file) {
+                if let None = self.get_piece_at(&(&p.rank + 1), to_file) {
+                    return true;
+                }
+            }
+        }
+        false
     }
 }
 
@@ -57,7 +128,7 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for rank in 0..STANDARD_BOARD_SIZE {
             for file in 0..STANDARD_BOARD_SIZE {
-                if let Some(p) = self.get_piece_at(rank.into(), file.into()) {
+                if let Some(p) = self.get_piece_at(&rank.into(), &file.into()) {
                     p.piece.fmt(f)?;
                 } else {
                     write!(f, " ")?;
