515aa5f35a3f82d8e60d139d3f90c32a7470ee0b
John Westhoff <johnjwesthoff@gmail.com>
2021-08-08 23:52:34 -0400

    Add pawnrank struct

---
+++ backend/src/pawn_rank.rs
@@ -0,0 +1,23 @@
+use std::{collections::HashSet, fmt};
+
+use crate::piece::{Color, Piece, PieceType};
+use num_bigint::BigInt;
+use num_traits::Signed;
+
+pub struct PawnRank {
+    moved: HashSet<BigInt>,
+}
+
+impl PawnRank {
+    pub fn new() -> Self {
+        Self {
+            moved: HashSet::new(),
+        }
+    }
+    pub fn has_moved(&self, file: &BigInt) -> bool {
+        self.moved.contains(file)
+    }
+    pub fn set_moved(&mut self, file: &BigInt) {
+        self.moved.insert(file.clone());
+    }
+}
