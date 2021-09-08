6e71b673259ba39c36b0d1fc1637136ff5babe7a
John Westhoff <johnjwesthoff@gmail.com>
2021-09-07 22:29:28 -0700

    Updated DB entries

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,12 +1,31 @@
 pub fn setup(conn: &mut Connection) -> std::io::Result<()> {
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS players (
-            name        TEXT PRIMARY KEY,
-            game_id     INTEGER,
-            correct     INTEGER,
-            drawer      INTEGER,
-            score       INTEGER
-              )",
+        "CREATE TABLE IF NOT EXISTS games (
+            id           INTEGER PRIMARY KEY,
+            white_castle INTEGER NOT NULL,
+            black_castle INTEGER NOT NULL,
+            turn         INTEGER NOT NULL
+         );
+         CREATE TABLE IF NOT EXISTS pieces (
+            id          INTEGER PRIMARY KEY,
+            game_id     INTEGER NOT NULL,
+            type        TEXT NOT NULL,
+            rank        TEXT NOT NULL,
+            file        TEXT NOT NULL,
+            start_rank  TEXT NOT NULL,
+            start_file  TEXT NOT NULL,
+            color       TEXT NOT NULL
+         );
+         CREATE TABLE IF NOT EXISTS moves (
+             id         INTEGER PRIMARY KEY,
+             game_id    INTEGER NOT NULL,
+             from_rank  TEXT NOT NULL,
+             to_rank    TEXT NOT NULL,
+             from_file  TEXT NOT NULL,
+             to_file    TEXT NOT NULL,
+             promotion  TEXT
+         );
+         ",
         params![],
     )
     .unwrap();
