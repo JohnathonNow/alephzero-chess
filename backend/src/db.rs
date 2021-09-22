d05c5911a466d08e245ccd347143e5794b83039d
John Westhoff <johnjwesthoff@gmail.com>
2021-09-22 16:56:16 -0700

    Change schema for simplicity

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -6,33 +6,17 @@ pub fn setup(conn: &mut Connection) -> std::io::Result<()> {
             black_castle INTEGER NOT NULL,
             turn         INTEGER NOT NULL
          );
-         CREATE TABLE IF NOT EXISTS pieces (
-            id          INTEGER PRIMARY KEY,
-            game_id     INTEGER NOT NULL,
-            type        TEXT NOT NULL,
-            rank        TEXT NOT NULL,
-            file        TEXT NOT NULL,
-            start_rank  TEXT NOT NULL,
-            start_file  TEXT NOT NULL,
-            color       TEXT NOT NULL
-         );
-         CREATE TABLE IF NOT EXISTS moves (
-             id         INTEGER PRIMARY KEY,
-             game_id    INTEGER NOT NULL,
-             from_rank  TEXT NOT NULL,
-             to_rank    TEXT NOT NULL,
-             from_file  TEXT NOT NULL,
-             to_file    TEXT NOT NULL,
-             promotion  TEXT
-         );
-         CREATE TABLE IF NOT EXISTS pawn_ranks (
-             id         INTEGER PRIMARY KEY,
-             game_id    INTEGER NOT NULL,
-             rank       TEXT NOT NULL
+         ",
+        params![],
+    ).unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS boards (
+            id           INTEGER PRIMARY KEY,
+            gameid       INTEGER NOT NULL,
+            board        TEXT
          );
          ",
         params![],
-    )
-    .unwrap();
+    ).unwrap();
     Ok(())
 }
