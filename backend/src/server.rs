91b00954d500fbc86b419875c5ca6b3f66a68e00
John Westhoff <johnjwesthoff@gmail.com>
2022-01-04 23:30:22 -0500

    Changed from busy waiting to condvar

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use std::sync::Arc;
-use std::sync::Mutex;
+#[cfg(feature = "server")]
+use async_std::sync::{Mutex, Condvar};
 use std::time::Duration;
 mod error;
 #[cfg(feature = "server")]
@@ -26,10 +27,13 @@ use crate::board_serializer::board_serialize;
 use crate::error::*;
 #[cfg(feature = "server")]
 use actix_files as fs;
-
+#[cfg(feature = "server")]
 use async_std::task;
 
+type SharedData = (Mutex<Board>, Condvar);
+type Shared = web::Data<SharedData>;
 
+#[cfg(feature = "server")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mut b = Board::new();
@@ -129,8 +133,7 @@ async fn main() -> std::io::Result<()> {
         7.into(),
         3.into(),
     ));
-
-    let board = web::Data::new(Arc::new(Mutex::new(b)));
+    let board: Shared = web::Data::new((Mutex::new(b), Condvar::new()));
     HttpServer::new(move || {
         App::new()
             .service(get)
@@ -149,41 +152,41 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[get("/board")]
-pub async fn get(board: web::Data<Arc<Mutex<Board>>>) -> Result<HttpResponse, Error> {
-    let b = board.lock().unwrap();
+pub async fn get(shared: Shared) -> Result<HttpResponse, Error> {
+    let (board, cvar) = &**shared;
+    let b = board.lock().await;
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .body(board_serialize(&b)))
 }
 
 #[get("/board/{version}")]
-pub async fn get_version(board: web::Data<Arc<Mutex<Board>>>, web::Path((version)): web::Path<(String)>) -> Result<HttpResponse, Error> {
+pub async fn get_version(shared: Shared, web::Path((version)): web::Path<(String)>) -> Result<HttpResponse, Error> {
+    let (board, cvar) = &**shared;
     let version2 = version.parse::<BigInt>().map_err(|_| Error::new())?;
-    loop {
-        let b = board.lock().unwrap();
-        if b.turn >= version2 {
-            return Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(board_serialize(&b)));
-        }
-        drop(b);
-        task::sleep(Duration::from_millis(10)).await;
+    let mut b = board.lock().await;
+    while  b.turn < version2 {
+       b = cvar.wait(b).await;
     }
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(board_serialize(&b)))
 }
 
 #[get("/legal/{px}/{py}/{wx}/{wy}/{zoom}")]
 pub async fn get_legal(
-    board: web::Data<Arc<Mutex<Board>>>,
+    shared: Shared,
     rules: web::Data<Arc<StandardChess>>,
     web::Path((px, py, wx, wy, zoom)): web::Path<(String, String, String, String, String)>,
 ) -> Result<HttpResponse, Error> {
+    let (board, cvar) = &**shared;
     let bigpx = BigInt::from_str(&px).map_err(|_| Error::new())?;
     let bigpy = BigInt::from_str(&py).map_err(|_| Error::new())?;
     let bigwx = BigInt::from_str(&wx).map_err(|_| Error::new())?;
     let bigwy = BigInt::from_str(&wy).map_err(|_| Error::new())?;
     let bigzoom = BigInt::from_str(&zoom).map_err(|_| Error::new())?;
 
-    let mut b = board.lock().unwrap();
+    let mut b = board.lock().await;
 
     let mut xx = bigwx.clone();
     let wwx = bigwx + bigzoom.clone();
@@ -206,22 +209,23 @@ pub async fn get_legal(
 
 #[get("/move/{px}/{py}/{dx}/{dy}")]
 pub async fn get_move(
-    board: web::Data<Arc<Mutex<Board>>>,
+    shared: Shared,
     rules: web::Data<Arc<StandardChess>>,
     web::Path((px, py, dx, dy)): web::Path<(String, String, String, String)>,
 ) -> Result<HttpResponse, Error> {
+    let (board, cvar) = &**shared;
     let bigpx = BigInt::from_str(&px).map_err(|_| Error::new())?;
     let bigpy = BigInt::from_str(&py).map_err(|_| Error::new())?;
     let bigdx = BigInt::from_str(&dx).map_err(|_| Error::new())?;
     let bigdy = BigInt::from_str(&dy).map_err(|_| Error::new())?;
 
-    let mut b = board.lock().unwrap();
+    let mut b = board.lock().await;
 
     if let Some(m) = Board::move_legal(&mut b, &rules, &bigpx, &bigpy, &bigdx, &bigdy) {
         b.do_move(m);
         b.turn += 1;
     }
-
+    cvar.notify_all();
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .body("swag"))
@@ -230,17 +234,18 @@ pub async fn get_move(
 
 #[get("/promote/{px}/{py}/{p}")]
 pub async fn get_promote(
-    board: web::Data<Arc<Mutex<Board>>>,
+    shared: Shared,
     rules: web::Data<Arc<StandardChess>>,
     web::Path((px, py, p)): web::Path<(String, String, String)>,
 ) -> Result<HttpResponse, Error> {
+    let (board, cvar) = &**shared;
     let bigpx = BigInt::from_str(&px).map_err(|_| Error::new())?;
     let bigpy = BigInt::from_str(&py).map_err(|_| Error::new())?;
 
-    let mut b = board.lock().unwrap();
+    let mut b = board.lock().await;
 
     b.promote(&bigpx, &bigpy, p);
-
+    cvar.notify_all();
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .body("swag"))
